55e426287ecb13afc9ee71aa6231afd7409d2a6a
tzm_remote <you@example.com>
2024-11-21 14:53:54 +0800

    change: deploy gpu device order

--- deploy_script/start_whisper_large_v3.sh
+++ deploy_script/start_whisper_large_v3.sh
@@ -5,10 +5,11 @@ source ~/.zshrc
 unset http_proxy
 unset https_proxy
 
-CUDA_VISIBLE_DEVICES=3 /app/server \
+CUDA_VISIBLE_DEVICES=1 /app/server \
 	-pc \
 	-pr \
-	--language chinese \
+	--language zh \
+	--prompt "以下是普通话的句子。" \
 	-m /models/alm/ggml/ggml-large-v3.bin \
 	--host 0.0.0.0 \
 	--port 8132
