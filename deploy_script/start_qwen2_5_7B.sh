5a6e69fdbd250fa4067dabcbc4347daf7c0d6d6f
tzm_remote <you@example.com>
2025-01-22 02:23:54 +0800

    style: refine deploy scripts

--- deploy_script/start_qwen2_5_7B.sh
+++ deploy_script/start_qwen2_5_7B.sh
@@ -5,12 +5,10 @@ conda activate vllm-infer
 unset http_proxy
 unset https_proxy
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 vllm serve /models/llm/Qwen/Qwen2___5-7B-Instruct \
+CUDA_VISIBLE_DEVICES=3 vllm serve /models/llm/Qwen/Qwen2___5-7B-Instruct \
 --host 0.0.0.0 \
 --port 8012 \
 --served-model-name ZhongjiaoGPT/Qwen \
---tensor-parallel-size 4 \
---gpu-memory-utilization 0.5 \
 --enable-auto-tool-choice \
 --tool-call-parser hermes \
 --chat-template /models/llm/Qwen/template.jinja
